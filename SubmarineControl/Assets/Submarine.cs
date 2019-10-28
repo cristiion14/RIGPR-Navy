7dd5cdc596ce557e7245781356f42c104f74ba57
cristiion14 <cristi.ion1996@gmail.com>
2019-10-28 15:05:04 +0000

    deleting test scene and adding the submarine control scene
    The first scene was a mock to test the integration of source control.
    
    Submarine control contains the scripts necessary for the submarine to float and move in the water, future improvements will follow

---
+++ SubmarineControl/Assets/Submarine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class Submarine : MonoBehaviour {
+    Rigidbody rb;
+    public float speed = 1f;
+    public float steerSpeed = 5f;
+    public float movementThreshold = 10f;
+    public float movementFactor;
+    public float steeringFactor;
+
+    public float waterLevel = 0f;
+    public float floatTreshold = 2f;
+    public float waterDensity = .125f;
+    public float downForce = 1.5f;
+
+    public float forceFactor;
+    public Vector3 floatForce;  
+
+	// Use this for initialization
+	void Start () {
+       rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	
+    void Update()
+    {
+     //   rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y, Input.GetAxisRaw("Vertical")*speed);
+    }
+
+    void FixedUpdate()
+    {
+        Float();
+    }
+    
+    void Float()
+    {
+        forceFactor = 1f - ((transform.position.y -waterLevel)/floatTreshold);
+        if(forceFactor>0)
+        {
+            floatForce = -Physics.gravity *GetComponent<Rigidbody>().mass* (forceFactor - GetComponent<Rigidbody>().velocity.y * waterDensity);
+            floatForce += new Vector3(0f, -downForce*GetComponent<Rigidbody>().mass, 0f);
+            GetComponent<Rigidbody>().AddForceAtPosition(floatForce, transform.position);
+        }
+    }
+    
+}
---
+++ SubmarineControl/Assets/SubmarineController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//to get the center of mass of any GM create an empty object and attach it to model on the top
+public class SubmarineController : MonoBehaviour {
+   public Vector3 COM;    //center of mass
+    Transform m_COM;
+    Submarine sub;
+    float verticalInput;
+    float horizontalInput;
+
+    void Start()
+    {
+        sub = new Submarine();
+    }
+    void Update()
+    {
+        Balance();
+        Movement();
+        Steer();
+    }
+
+    void Balance()
+    {
+        //control the balance of the sub
+        if(!m_COM)
+        {
+            m_COM = new GameObject("COM").transform;
+            m_COM.SetParent(transform);
+            
+        }
+        m_COM.position = COM;
+        GetComponent<Rigidbody>().centerOfMass = m_COM.position;
+    }
+    void Movement()
+    {
+        verticalInput = Input.GetAxisRaw("Vertical");
+        sub.movementFactor = Mathf.Lerp(sub.movementFactor, verticalInput/2, Time.deltaTime / sub.movementThreshold);
+        transform.Translate(0f, 0f, sub.movementFactor * sub.speed);
+    }
+    void Steer()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        sub.steeringFactor = Mathf.Lerp(sub.steeringFactor,horizontalInput/4 , Time.deltaTime / sub.movementThreshold);
+        transform.Rotate(0f,sub.steeringFactor * sub.steerSpeed, 0f);
+    }
+}
